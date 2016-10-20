fa577c31bd35262a8b60b4c711d16fb697f29334
Irony Man <ironyman777@gmail.com>
2016-10-20 15:33:27 -0400

    Update main.js

--- Data/main.js
+++ Data/main.js
@@ -2,7 +2,7 @@ var sketchProc=function(processingInstance){ with (processingInstance){
 /*@pjs preload="Character concept 2.png","Ship1.png";*/
 // Stay dangereous.
 setup = function() {
-	var charx = 400;
+	/*var charx = 400;
 	var chary = 300;
 
 	var keys = [];
@@ -12,7 +12,7 @@ setup = function() {
 	keyReleased = function() {
 		keys[keyCode] = false;
 	};
-	
+	*/
 	img1=loadImage("Images/char1.jpg");
 	size(800,600);
 	frameRate(60);
@@ -21,11 +21,11 @@ setup = function() {
 
 draw = function() {
 	background(255);
-	var a = atan2(chary-mouseY,charx-mouseX);
+	/*var a = atan2(chary-mouseY,charx-mouseX);
 	pushMatrix();
 		translate(charx,chary);
-		rotate(a+90);
+		rotate(a+90);*/
 	image(img1,0,0);
-	popMatrix();
+	//popMatrix();
 };
 }};
