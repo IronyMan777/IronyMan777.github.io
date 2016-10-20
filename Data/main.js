a9eedfb39c623368746fde715f2ddc141fbfdcb4
Irony Man <ironyman777@gmail.com>
2016-10-20 15:49:00 -0400

    Update main.js

--- Data/main.js
+++ Data/main.js
@@ -10,7 +10,55 @@ setup = function() {
 	size(800,600);
 	frameRate(60);
 	angleMode = "degrees";
-};
+	
+	charspeed = 2;
+	movstyle = "WASD";
+	
+	var charl = function() {
+	    charx -= charspeed;
+	};
+	var charr = function() {
+	    charx += charspeed;
+	};
+	var charu = function() {
+	    chary -= charspeed;
+	};
+	var chard = function() {
+	    chary += charspeed;
+	};
+	var charmovetopdown = function() {
+	    // WASD keys
+	    if (movstyle === "WASD") {
+		if (keys[65]) {
+		    charl();
+		}
+		if (keys[68]) {
+		    charr();
+		}
+		if (keys[87]) {
+		    charu();
+		}
+		if (keys[83]) {
+		    chard();
+		}
+	    }
+	    if (movstyle === "arrows") {
+		// Arrow keys
+		if (keys[37]) {
+		    charl();
+		}
+		if (keys[39]) {
+		    charr();
+		}
+		if (keys[38]) {
+		    charu();
+		}
+		if (keys[40]) {
+		    chard();
+		}
+	    }
+	};
+	};
 
 draw = function() {
 	background(240);
@@ -20,6 +68,7 @@ draw = function() {
 		rotate(a+225);
 	image(img1,-50,-50);
 	popMatrix();
+	charmovetopdown();
 };
 keyPressed = function() {
 	keys[keyCode] = true;
