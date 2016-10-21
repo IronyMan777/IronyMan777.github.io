2257227268f06da3eee2f72c05604b9ad8171f31
Irony Man <ironyman777@gmail.com>
2016-10-21 12:05:00 -0400

    Update main.js

--- Data/main.js
+++ Data/main.js
@@ -6,8 +6,10 @@ setup = function() {
 	charx = 400;
 	chary = 300;
 	chara = 0;
+	moving = false;
 	
 	img1=loadImage("Data/Images/char2.png");
+	walking = loadImage("Data/Images/char3.gif");
 	size(800,600);
 	frameRate(60);
 	angleMode = "degrees";
@@ -16,36 +18,45 @@ setup = function() {
 	movstyle = "WASD";
 	
 	charmovetopdown = function() {
-	    // WASD keys
-	    if (movstyle == "WASD") {
-		if (keys[65]) {
-		    charx-=charspeed;
+	    	// WASD keys
+		moving = false;
+		if (movstyle == "WASD") {
+			if (keys[65]) {
+			    charx-=charspeed;
+				moving = true;
+			}
+			if (keys[68]) {
+			    charx+=charspeed;
+				moving = true;
+			}
+			if (keys[87]) {
+			    chary-=charspeed;
+				moving = true;
+			}
+			if (keys[83]) {
+			    chary+=charspeed;
+				moving = true;
+			}
 		}
-		if (keys[68]) {
-		    charx+=charspeed;
+		if (movstyle == "arrows") {
+			// Arrow keys
+			if (keys[37]) {
+			    charx-=charspeed;
+				moving = true;
+			}
+			if (keys[39]) {
+			    charx+=charspeed;
+				moving = true;
+			}
+			if (keys[38]) {
+			    chary-=charspeed;
+			    moving = true;
+			}
+			if (keys[40]) {
+			    chary+=charspeed;
+			    moving = true;
+			}
 		}
-		if (keys[87]) {
-		    chary-=charspeed;
-		}
-		if (keys[83]) {
-		    chary+=charspeed;
-		}
-	    }
-	    if (movstyle == "arrows") {
-		// Arrow keys
-		if (keys[37]) {
-		    charx-=charspeed;
-		}
-		if (keys[39]) {
-		    charx+=charspeed;
-		}
-		if (keys[38]) {
-		    chary-=charspeed
-		}
-		if (keys[40]) {
-		    chary+=charspeed
-		}
-	    }
 	};
 };
 
@@ -57,7 +68,11 @@ draw = function() {
 	pushMatrix();
 		translate(charx,chary);
 		rotate(chara+225);
-	image(img1,-50,-50);
+	if (moving == false) {
+		image(img1,-50,-50);
+	} else if (moving == true) {
+		image(walking,-50,-50);
+	}
 	popMatrix();
 	charmovetopdown();
 };
