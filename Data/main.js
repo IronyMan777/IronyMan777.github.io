cf75ed9a853839cf69327a8effa29c949cd629ab
Irony Man <ironyman777@gmail.com>
2016-11-03 15:53:28 -0400

    Update main.js

--- Data/main.js
+++ Data/main.js
@@ -28,7 +28,7 @@ setup = function() {
 		//chara = atan2(y-mouseY,x-mouseX);
 		pushMatrix();
 		translate(x,y);
-		rotate(chara+150);
+		rotate(chara+120);
 			if (moving == true) {
 				aminate(charwalk,-50,-50,1);
 			} else {
