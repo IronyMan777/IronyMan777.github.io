64313ff2e9909690b4d017a2c70de3c1897a0beb
Irony Man <ironyman777@gmail.com>
2016-11-14 15:00:43 -0500

    Update main.js

--- Data/main.js
+++ Data/main.js
@@ -2,25 +2,35 @@ var sketchProc=function(processingInstance){ with (processingInstance){
 // Stay dangereous.
 	keys = [];
 setup = function() {
-	charx = 400;
-	chary = 500;
-	chartx = 400;
-	charty = 300;
-	chardir = "right";
+	
 	mouseIsPressed = false;
 	
-	charvertspeed = 0;
-	jumping = false;
+	lefthand = null;
+	righthand = null;
+	
+	
 	
 	grav = 3;
 	
 	bullets = [];
-	chara = 0;
-	chara2 = 0;
-	moving = false;
-	aiming = false;
-	charshooting = false;
-	characcuracy = 3;
+	char = {
+		x: 400,
+		y: 500,
+		tx: 400,
+		ty: 300,
+		speed: 4,
+		dir: "right",
+		a: 0,
+		a2: 0,
+		moving: false,
+		aiming: false,
+		shootingright: false,
+		shootingleft: false,
+		accuracy: 3,
+		
+		vertspeed: 0,
+		jumping: false
+	};
 	
 	frame = 0;
 	
@@ -28,28 +38,51 @@ setup = function() {
 	frameRate(60);
 	angleMode = "degrees";
 	
-	charspeed = 4;
-	movstyle = "WASD";	
 	
+	movstyle = "WASD";
 	
-	charwalk = [loadImage("Data/Images/chartd1.png"),loadImage("Data/Images/chartd2.png"),loadImage("Data/Images/chartd3.png"),loadImage("Data/Images/chartd4.png")];
+	// Objects!
+	item = function(type,hands,power,image) {
+		if (type == "gun") {
+		
+		}
+		
+		if (type == "melee") {
+		
+		}
+	};
+	
+	pistol = function(x,y) {
+		item();
+		
+	};
+	
+	loadamination = function(name,number) {
+		pictures = [];
+		for (var q=0;q<number;q++) {
+			pictures[q-1] = loadImage("Data/Images/"+name+q+".png");
+		}
+		return pictures;
+	};
+	
+	charwalk = loadamination("chartd",5);
 	
 	shoot = function(x,y,tx,ty,speed,mode) {
 		//fill(255,255,245,230);
-		bullets.push({x,y,tx,ty,speed});
+		bullets.push({x,y,angle:atan2(y-ty,x-tx),speed});
 		
 	};
 	drawbullets = function() {
 		for (b = 0; b < bullets.length; b++) {
 			fill(250,255,230);
-			if (dist(bullets[b].x,bullets[b].y,bullets[b].tx,bullets[b].ty) > bullets[b].speed) {
+			/*if (dist(bullets[b].x,bullets[b].y,bullets[b].tx,bullets[b].ty) > bullets[b].speed) {*/
 				ellipse(bullets[b].x,bullets[b].y,5,5);
-			}
-			bullets[b].x -= bullets[b].speed*cos(atan2(bullets[b].y-bullets[b].ty,bullets[b].x-bullets[b].tx));
-			bullets[b].y -= bullets[b].speed*sin(atan2(bullets[b].y-bullets[b].ty,bullets[b].x-bullets[b].tx));
+			//}
+			bullets[b].x -= bullets[b].speed*cos(bullets[b].angle);
+			bullets[b].y -= bullets[b].speed*sin(bullets[b].angle);
 			
-			bullets[b].tx -= bullets[b].speed*cos(atan2(bullets[b].y-bullets[b].ty,bullets[b].x-bullets[b].tx));
-			bullets[b].ty -= bullets[b].speed*sin(atan2(bullets[b].y-bullets[b].ty,bullets[b].x-bullets[b].tx));
+			bullets[b].tx -= bullets[b].speed*cos(bullets[b].angle);
+			bullets[b].ty -= bullets[b].speed*sin(bullets[b].angle);
 			
 			//For debug purposes and safety of the universe...
 			if (bullets[b].x>width||bullets[b].x<0||bullets[b].y>height||bullets[b].y<0) {
@@ -71,27 +104,27 @@ setup = function() {
 		image(imagen[floor(frame)],x,y);
 	};
 	
-	char = function(x,y,speed,maxhealth,power,mode) {
+	displaychar = function(x,y,speed,maxhealth,power,mode) {
 		if (mode == "topdown") {
 		
 			pushMatrix();
 			translate(x,y);
 			
-			if (aiming) {
-				rotate(chara2+radians(270));
+			if (char.aiming) {
+				rotate(char.a2+radians(270));
 			} else {
-				rotate(chara+radians(270));
+				rotate(char.a+radians(270));
 			}
-			if (moving) {
+			if (char.moving) {
 				aminate(charwalk,-50,-50,0.1);
 			} else {
 				image(charwalk[0],-50,-50);
 			}
 			popMatrix();
 		
-			if (aiming) {
-				if (charshooting) {
-					shoot(charx,chary,mouseX+random(-characcuracy,characcuracy),mouseY+random(-characcuracy,characcuracy),random(9,11));
+			if (char.aiming) {
+				if (char.shootingleft) {
+					shoot(char.x,char.y,mouseX+random(-char.accuracy,char.accuracy),mouseY+random(-char.accuracy,char.accuracy),random(9,11));
 				}
 			}
 		} else if (mode == "platform") {
@@ -107,148 +140,149 @@ setup = function() {
 			popMatrix();
 		}
 	};
-	
-	/*loadamination = function(name,number) {
-		images = [];
-		for (var q=0;q<number;q++) {
-			filename = name + nf(q,4) + ".png";
-			images[q] = loadImage(filename);
-		}
-		return images[];
-	};*/
-	
 	charmoveplatorm = function() {
-		moving = false;
+		char.moving = false;
 		if (keyPressed) {
 			if (movstyle == "WASD") {
 				if (keys[65]) {
-					charx-=charspeed;
-					moving = true;
-					chardir = "left";
+					char.x-=char.speed;
+					char.moving = true;
+					char.dir = "left";
 				}
 				if (keys[68]) {
-					charx+=charspeed;
-					moving = true;
-					chardir = "right";
+					char.x+=char.speed;
+					char.moving = true;
+					char.dir = "right";
 				}
 				if (keys[87]) {
-					if (jumping == false) {
-						charvertspeed -= 10-grav; 
-						chary -= charvertspeed;
-						jumping = true;
+					if (char.jumping == false) {
+						char.vertspeed -= 10-grav; 
+						char.y -= char.vertspeed;
+						char.jumping = true;
 					}
 				}
 			}			
 		}
-		chary += grav;
-		if (chary > height) {
-			chary -= grav;
-			jumping = false;
-			charvertspeed = 0;
+		char.y += grav;
+		if (char.y > height) {
+			char.y -= grav;
+			char.jumping = false;
+			char.vertspeed = 0;
 		}		
 	};
 	
 	charmovetopdown = function() {		
-		moving = false;
-		//aiming;
-		chara = atan2(chary-charty,charx-chartx);
-		chara2 = atan2(chary-mouseY,charx-mouseX);
-		if (mouseIsPressed==true) {
-			if (mouseButton == RIGHT) {
-				aiming = true;
-			}
-			if (mouseButton == LEFT) {
-				charshooting = true;
-			}			
+		char.moving = false;
+		//char.aiming;
+		char.a = atan2(char.y-char.ty,char.x-char.tx);
+		char.a2 = atan2(char.y-mouseY,char.x-mouseX);
+		if (keys[32] == true) {
+			char.aiming = true;
+			if (mouseIsPressed) {
+				if (mouseButton == RIGHT) {
+					char.shootingright = true;
+				} else {
+					char.shootingright = false;
+				}
+				if (mouseButton == LEFT) {
+					char.shootingleft = true;
+				} else {
+					char.shootingleft = false;
+				}
+			} else {
+				char.shootingright = false;
+				char.shootingleft = false;
+			}	
 		} else {
-			aiming = false;
-			charshooting = false;
+			char.aiming = false;		
+			char.shootingright = false;
+			char.shootingleft = false;
 		}
 		// WASD keys
 		if (keyPressed) {
 			if (movstyle == "WASD") {
 				if (keys[65]) {
-				    chartx-=charspeed;
-					moving = true;
+				    char.tx-=char.speed;
+					char.moving = true;
 				}
 				if (keys[68]) {
-				    chartx+=charspeed;
-					moving = true;
+				    char.tx+=char.speed;
+					char.moving = true;
 				}
 				if (keys[87]) {
-				    charty-=charspeed;
-					moving = true;
+				    char.ty-=char.speed;
+					char.moving = true;
 				}
 				if (keys[83]) {
-				    charty+=charspeed;
-					moving = true;
+				    char.ty+=char.speed;
+					char.moving = true;
 				}
 			}
 			if (movstyle == "arrows") {
 				// Arrow keys
 				if (keys[37]) {
-				    chartx-=charspeed;
-					moving = true;
+				    char.tx-=char.speed;
+					char.moving = true;
 				}
 				if (keys[39]) {
-				    chartx+=charspeed;
-					moving = true;
+				    char.tx+=char.speed;
+					char.moving = true;
 				}
 				if (keys[38]) {
-				    charty-=charspeed;
-				    moving = true;
+				    char.ty-=char.speed;
+				    char.moving = true;
 				}
 				if (keys[40]) {
-				    charty+=charspeed;
-				    moving = true;
+				    char.ty+=char.speed;
+				    char.moving = true;
 				}
 			}
 		}
 		
-		if (moving == false) {
-			if (chartx > charx-cos(chara)*charspeed) {
-				chartx -= charspeed;
+		if (char.moving == false) {
+			if (char.tx > char.x-cos(char.a)*char.speed) {
+				char.tx -= char.speed;
 			}
-			if (chartx < charx+cos(chara)*charspeed) {
-				chartx += charspeed;
+			if (char.tx < char.x+cos(char.a)*char.speed) {
+				char.tx += char.speed;
 			}
-			if (charty > chary-sin(chara)*charspeed) {
-				charty -= charspeed;
+			if (char.ty > char.y-sin(char.a)*char.speed) {
+				char.ty -= char.speed;
 			}
-			if (charty < chary+sin(chara)*charspeed) {
-				charty += charspeed;
+			if (char.ty < char.y+sin(char.a)*char.speed) {
+				char.ty += char.speed;
 			}
 		}
-		/*if (chartx > charx+(3*charspeed)) {
-			chartx -= charspeed;
+		/*if (char.tx > char.x+(3*char.speed)) {
+			char.tx -= char.speed;
 		}
-		if (chartx < charx-(3*charspeed)) {
-			chartx += charspeed;
+		if (char.tx < char.x-(3*char.speed)) {
+			char.tx += char.speed;
 		}
-		if (charty > chary+(3*charspeed)) {
-			charty -= charspeed;
+		if (char.ty > char.y+(3*char.speed)) {
+			char.ty -= char.speed;
 		}
-		if (charty < chary-(3*charspeed)) {
-			charty += charspeed;
+		if (char.ty < char.y-(3*char.speed)) {
+			char.ty += char.speed;
 		}*/
-		if (dist(charx,chary,chartx,charty) > charspeed*6) {
-			chartx+=cos(chara)*(charspeed);
-			charty+=sin(chara)*(charspeed);
+		if (dist(char.x,char.y,char.tx,char.ty) > char.speed*6) {
+			char.tx+=cos(char.a)*(char.speed);
+			char.ty+=sin(char.a)*(char.speed);
 		}
 		
-		if (moving == true) {		
-			charx -= cos(chara)*charspeed;
-			chary -= sin(chara)*charspeed;
+		if (char.moving == true) {		
+			char.x -= cos(char.a)*char.speed;
+			char.y -= sin(char.a)*char.speed;
 		}
 		/*fill(255);
-		ellipse(chartx,charty,10,10);
+		ellipse(char.tx,char.ty,10,10);
 		fill(255,0,0);
-		ellipse(charx,chary,10,10);*/
+		ellipse(char.x,char.y,10,10);*/
 	};
 	/*level = function(mode) {
 		background(100);	
 		charmovetopdown();
-		char(charx,chary,0,charspeed,100,10,mode);
+		char(char.x,char.y,0,char.speed,100,10,mode);
 		drawbullets();
 	};
 	
@@ -260,10 +294,9 @@ setup = function() {
 
 draw = function() {
 	background(100);	
-	charmoveplatorm();
-	char(charx,chary,charspeed,100,10,"platform");
-	
+	charmovetopdown();
 	drawbullets();
+	displaychar(char.x,char.y,char.speed,100,10,"topdown");	
 };
 
 keyPressed = function() {
