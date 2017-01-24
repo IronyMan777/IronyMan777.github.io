3c6f720fd3881b78f357a593ffe48673125fe2f4
Irony Man <ironyman777@gmail.com>
2017-01-24 17:54:22 -0500

    Update main.js

--- Data/main.js
+++ Data/main.js
@@ -1,15 +1,16 @@
-class button {
-	int x;
-	int y;
-	int width;
-	int height;
-	string text;
+button = function(con) {
+	int x = con.x;
+	int y = con.y;
+	int width = con.width;
+	int height = con.height;
+	string text = con.text;
 };
 
 setup = function() {
-	
+	startbutton = new button({x:width/2,y:height/2,width:150,height:80,text:"Start"});
 };
 
 draw = function() {
 	background(10,50,50);
+	
 };
